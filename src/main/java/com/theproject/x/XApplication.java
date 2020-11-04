1e8111b6ddac30613005c7c4447d30fedf4037a6
Rafail Kiloudis <r.kiloudis@gmail.com>
2020-11-04 14:07:35 +0200

    Improvements

--- src/main/java/com/theproject/x/XApplication.java
+++ src/main/java/com/theproject/x/XApplication.java
@@ -2,6 +2,8 @@ package com.theproject.x;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class XApplication {
@@ -10,4 +12,9 @@ public class XApplication {
 		SpringApplication.run(XApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+	    return new RestTemplate();
+	}
+	
 }
